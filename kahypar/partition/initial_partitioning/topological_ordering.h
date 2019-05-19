58626d86cc286a5174dfc50ba86f9b666354c8a5
Daniel Seemaier <daniel.seemaier@googlemail.com>
2019-05-19 20:52:56 +0200

    Refactor topological ordering IP algorithm to use the standalone topological ordering algorithm implementation

--- kahypar/partition/initial_partitioning/topological_ordering.h
+++ kahypar/partition/initial_partitioning/topological_ordering.h
@@ -6,6 +6,7 @@
 #include "kahypar/partition/initial_partitioning/i_initial_partitioner.h"
 #include "kahypar/partition/initial_partitioning/initial_partitioner_base.h"
 #include "kahypar/utils/randomize.h"
+#include "kahypar/dag/topological_ordering.h"
 
 namespace kahypar {
 class TopologicalOrderingInitialPartitioner : public IInitialPartitioner,
@@ -26,7 +27,7 @@ class TopologicalOrderingInitialPartitioner : public IInitialPartitioner,
   TopologicalOrderingInitialPartitioner& operator= (TopologicalOrderingInitialPartitioner&&) = delete;
 
  private:
-  void partitionImpl() override final {
+  void partitionImpl() final {
     Base::multipleRunsInitialPartitioning();
   }
 
@@ -35,58 +36,16 @@ class TopologicalOrderingInitialPartitioner : public IInitialPartitioner,
     _context.initial_partitioning.unassigned_part = -1;
     Base::resetPartitioning();
 
-    std::vector<HypernodeID> position(_hg.currentNumNodes());
-    for (const HyperedgeID &he : _hg.edges()) {
-      for (const HypernodeID &hn : _hg.heads(he)) {
-        position[hn] += _hg.edgeNumTails(he);
+    auto topological_ordering = dag::calculateTopologicalOrdering(_hg);
+
+    // assign nodes to the lowest non-overloaded block in topological order
+    PartitionID p = 0;
+    for (const HypernodeID& hn : topological_ordering) {
+      while (!Base::assignHypernodeToPartition(hn, p)) {
+        ++p;
+        ASSERT(p < _context.partition.k, "Could not find a non-overloaded partition for node" << hn);
       }
     }
-
-    std::vector<HypernodeID> candidates;
-    for (const HypernodeID &hn : _hg.nodes()) {
-      if (position[hn] == 0) {
-        candidates.push_back(hn);
-      }
-    }
-
-    HypernodeID target_block_size = _hg.currentNumNodes() / _context.partition.k;
-    HypernodeID current_block_size = 0;
-    PartitionID current_block = 0;
-
-    while (!candidates.empty()) {
-      // select random candidate and remove it from the candidates list
-      HypernodeID u = Randomize::instance().popRandomElement(candidates);
-
-      // assign node to the current block
-      if (current_block_size > target_block_size) {
-        current_block = std::min(_context.partition.k - 1, current_block + 1);
-        current_block_size = 0;
-      }
-      _hg.setNodePart(u, current_block);
-      ++current_block_size;
-
-      // decrease position of heads of edges that contain u as tail
-      for (const HyperedgeID &he : _hg.incidentEdges(u)) {
-        if (!_hg.isTail(u, he)) continue;
-
-        for (const HypernodeID &hn : _hg.heads(he)) {
-          ASSERT(position[hn] > 0);
-          --position[hn];
-          if (position[hn] == 0) {
-            candidates.push_back(hn);
-          }
-        }
-      }
-    }
-
-    ASSERT([&]() {
-      for (const HypernodeID& hn : _hg.nodes()) {
-        if (_hg.partID(hn) == -1) {
-          return false;
-        }
-      }
-      return true;
-    } (), "There are unassigned hypernodes -- the graph contains cycles!");
   }
 };
 } // namespace kahypar
