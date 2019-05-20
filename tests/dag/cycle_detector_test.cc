9031c402d430e7f9cb918c6e519de8600b9edf41
Daniel Seemaier <daniel.seemaier@googlemail.com>
2019-05-20 07:05:05 +0200

    Rename private test functions

--- tests/dag/cycle_detector_test.cc
+++ tests/dag/cycle_detector_test.cc
@@ -10,7 +10,7 @@ using ::testing::ContainerEq;
 
 namespace kahypar {
 template<typename Detector>
-static void _simple_cycle_test() {
+static void _simpleCycleTest() {
   Detector detector(5);
   // build a path: 0 -> 1 -> 2 -> 3 -> 4
   ASSERT_TRUE(detector.connect(0, 1));
@@ -34,7 +34,7 @@ static void _simple_cycle_test() {
 }
 
 template<typename Detector>
-static void _another_simple_cycle_test() {
+static void _anotherSimpleCycleTest() {
   Detector detector(5);
 
   //   /> 3 <\
@@ -61,14 +61,14 @@ static void _another_simple_cycle_test() {
 }
 
 template<typename Detector>
-static void _reject_self_loops_test() {
+static void _rejectSelfLoopTest() {
   Detector detector(2);
   ASSERT_FALSE(detector.connect(0, 0));
   ASSERT_FALSE(detector.connect(1, 1));
 }
 
 template<typename Detector, std::size_t N = 512>
-static void _big_simple_cycle_test() {
+static void _bigSimpleCycleTest() {
   Detector detector(N);
   for (std::size_t i = 0; i < N - 1; ++i) {
     ASSERT_TRUE(detector.connect(i, i + 1));
@@ -77,7 +77,7 @@ static void _big_simple_cycle_test() {
 }
 
 template<typename Detector, std::size_t N = 128>
-static void _big_tree_test() {
+static void _bigTreeTest() {
   Detector detector(N);
 
   for (std::size_t u = 0; u < N; ++u) {
@@ -92,7 +92,7 @@ static void _big_tree_test() {
 }
 
 template<typename Detector, std::size_t N = 512, std::size_t M = 1024, int seed = 0>
-static void _random_test() {
+static void _randomGraphTest() {
   Detector detector(N);
   DFSCycleDetector reference_detector(N);
 
@@ -122,31 +122,31 @@ static void _random_test() {
 }
 
 // DFSCycle Detector
-TEST(DFS_CYCLE_DETECTOR, SimpleCycle) { _simple_cycle_test<DFSCycleDetector>(); }
-TEST(DFS_CYCLE_DETECTOR, AnotherSimpleCycle) { _another_simple_cycle_test<DFSCycleDetector>(); }
-TEST(DFS_CYCLE_DETECTOR, RejectsSelfLoops) { _reject_self_loops_test<DFSCycleDetector>(); }
-TEST(DFS_CYCLE_DETECTOR, BigSimpleCycle) { _big_simple_cycle_test<DFSCycleDetector>(); }
-TEST(DFS_CYCLE_DETECTOR, BigTree) { _big_tree_test<DFSCycleDetector>(); }
+TEST(DFS_CYCLE_DETECTOR, SimpleCycle) { _simpleCycleTest<DFSCycleDetector>(); }
+TEST(DFS_CYCLE_DETECTOR, AnotherSimpleCycle) { _anotherSimpleCycleTest<DFSCycleDetector>(); }
+TEST(DFS_CYCLE_DETECTOR, RejectsSelfLoops) { _rejectSelfLoopTest<DFSCycleDetector>(); }
+TEST(DFS_CYCLE_DETECTOR, BigSimpleCycle) { _bigSimpleCycleTest<DFSCycleDetector>(); }
+TEST(DFS_CYCLE_DETECTOR, BigTree) { _bigTreeTest<DFSCycleDetector>(); }
 
 // KahnCycleDetector
-TEST(KAHN_CYCLE_DETECTOR, SimpleCycle) { _simple_cycle_test<KahnCycleDetector>(); }
-TEST(KAHN_CYCLE_DETECTOR, AnotherSimpleCycle) { _another_simple_cycle_test<KahnCycleDetector>(); }
-TEST(KAHN_CYCLE_DETECTOR, RejectsSelfLoops) { _reject_self_loops_test<KahnCycleDetector>(); }
-TEST(KAHN_CYCLE_DETECTOR, BigSimpleCycle) { _big_simple_cycle_test<KahnCycleDetector>(); }
-TEST(KAHN_CYCLE_DETECTOR, BigTree) { _big_tree_test<KahnCycleDetector>(); }
-TEST(KAHN_CYCLE_DETECTOR, Random) { _random_test<KahnCycleDetector, 128, 256, 128>(); }
-TEST(KAHN_CYCLE_DETECTOR, Random1) { _random_test<KahnCycleDetector, 256, 512, 256>(); }
-TEST(KAHN_CYCLE_DETECTOR, Random2) { _random_test<KahnCycleDetector, 512, 512, 512>(); }
-TEST(KAHN_CYCLE_DETECTOR, Random3) { _random_test<KahnCycleDetector>(); }
+TEST(KAHN_CYCLE_DETECTOR, SimpleCycle) { _simpleCycleTest<KahnCycleDetector>(); }
+TEST(KAHN_CYCLE_DETECTOR, AnotherSimpleCycle) { _anotherSimpleCycleTest<KahnCycleDetector>(); }
+TEST(KAHN_CYCLE_DETECTOR, RejectsSelfLoops) { _rejectSelfLoopTest<KahnCycleDetector>(); }
+TEST(KAHN_CYCLE_DETECTOR, BigSimpleCycle) { _bigSimpleCycleTest<KahnCycleDetector>(); }
+TEST(KAHN_CYCLE_DETECTOR, BigTree) { _bigTreeTest<KahnCycleDetector>(); }
+TEST(KAHN_CYCLE_DETECTOR, Random) { _randomGraphTest<KahnCycleDetector, 128, 256, 128>(); }
+TEST(KAHN_CYCLE_DETECTOR, Random1) { _randomGraphTest<KahnCycleDetector, 256, 512, 256>(); }
+TEST(KAHN_CYCLE_DETECTOR, Random2) { _randomGraphTest<KahnCycleDetector, 512, 512, 512>(); }
+TEST(KAHN_CYCLE_DETECTOR, Random3) { _randomGraphTest<KahnCycleDetector>(); }
 
 // PseudoTopologicalOrderingCycleDetector
-TEST(PSEUDO_TOPO_CYCLE_DETECTOR, SimpleCycle) { _simple_cycle_test<PseudoTopologicalOrderingCycleDetector>(); }
-TEST(PSEUDO_TOPO_CYCLE_DETECTOR, AnotherSimpleCycle) { _another_simple_cycle_test<PseudoTopologicalOrderingCycleDetector>(); }
-TEST(PSEUDO_TOPO_CYCLE_DETECTOR, RejectsSelfLoops) { _reject_self_loops_test<PseudoTopologicalOrderingCycleDetector>(); }
-TEST(PSEUDO_TOPO_CYCLE_DETECTOR, BigSimpleCycle) { _big_simple_cycle_test<PseudoTopologicalOrderingCycleDetector>(); }
-TEST(PSEUDO_TOPO_CYCLE_DETECTOR, BigTree) { _big_tree_test<PseudoTopologicalOrderingCycleDetector>(); }
-TEST(PSEUDO_TOPO_CYCLE_DETECTOR, Random) { _random_test<PseudoTopologicalOrderingCycleDetector, 128, 256, 128>(); }
-TEST(PSEUDO_TOPO_CYCLE_DETECTOR, Random1) { _random_test<PseudoTopologicalOrderingCycleDetector, 256, 512, 256>(); }
-TEST(PSEUDO_TOPO_CYCLE_DETECTOR, Random2) { _random_test<PseudoTopologicalOrderingCycleDetector, 512, 512, 512>(); }
-TEST(PSEUDO_TOPO_CYCLE_DETECTOR, Random3) { _random_test<PseudoTopologicalOrderingCycleDetector>(); }
+TEST(PSEUDO_TOPO_CYCLE_DETECTOR, SimpleCycle) { _simpleCycleTest<PseudoTopologicalOrderingCycleDetector>(); }
+TEST(PSEUDO_TOPO_CYCLE_DETECTOR, AnotherSimpleCycle) { _anotherSimpleCycleTest<PseudoTopologicalOrderingCycleDetector>(); }
+TEST(PSEUDO_TOPO_CYCLE_DETECTOR, RejectsSelfLoops) { _rejectSelfLoopTest<PseudoTopologicalOrderingCycleDetector>(); }
+TEST(PSEUDO_TOPO_CYCLE_DETECTOR, BigSimpleCycle) { _bigSimpleCycleTest<PseudoTopologicalOrderingCycleDetector>(); }
+TEST(PSEUDO_TOPO_CYCLE_DETECTOR, BigTree) { _bigTreeTest<PseudoTopologicalOrderingCycleDetector>(); }
+TEST(PSEUDO_TOPO_CYCLE_DETECTOR, Random) { _randomGraphTest<PseudoTopologicalOrderingCycleDetector, 128, 256, 128>(); }
+TEST(PSEUDO_TOPO_CYCLE_DETECTOR, Random1) { _randomGraphTest<PseudoTopologicalOrderingCycleDetector, 256, 512, 256>(); }
+TEST(PSEUDO_TOPO_CYCLE_DETECTOR, Random2) { _randomGraphTest<PseudoTopologicalOrderingCycleDetector, 512, 512, 512>(); }
+TEST(PSEUDO_TOPO_CYCLE_DETECTOR, Random3) { _randomGraphTest<PseudoTopologicalOrderingCycleDetector>(); }
 } // namespace kahypar
