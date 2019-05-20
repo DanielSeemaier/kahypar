5d3109be98cb940103e8b2d2379d3f0cedc23f3a
Daniel Seemaier <daniel.seemaier@googlemail.com>
2019-05-20 10:08:38 +0200

    Add more quotient graph tests

--- tests/dag/quotient_graph_test.cc
+++ tests/dag/quotient_graph_test.cc
@@ -20,21 +20,41 @@ static void _applyAcyclic4Partition(Hypergraph &hg) {
   hg.setNodePart(5, 3);
 }
 
-/*static void _applyCyclic4Partition(Hypergraph &hg) {
-  hg.changeK(4);
-  hg.setNodePart(6, 0);
-  hg.setNodePart(0, 0);
-  hg.setNodePart(1, 2);
-  hg.setNodePart(2, 1);
-  hg.setNodePart(3, 2);
-  hg.setNodePart(4, 2);
-  hg.setNodePart(5, 3);
-}*/
+static void _partitionUsingTopologicalOrdering(Hypergraph& hg, const PartitionID k) {
+  auto ordering = calculateTopologicalOrdering(hg);
+  PartitionID part = 0;
+  HypernodeID nodes_per_part = hg.currentNumPins() / k;
+  HypernodeID nodes_in_cur_part = 0;
+
+  hg.changeK(k);
+  for (const HypernodeID& hn : ordering) {
+    hg.setNodePart(hn, part);
+    if (nodes_in_cur_part == nodes_per_part) {
+      ++part;
+      nodes_in_cur_part = 0;
+    }
+  }
+}
 
 static Context _createContext(PartitionID k) {
   Context ctx;
   ctx.partition.k = k;
   return ctx;
+}
+
+template<typename Detector>
+static void _performMonkeyMoves(Hypergraph& hg, QuotientGraph<Detector>& qg, const std::size_t k, const std::size_t N) {
+  Randomize& random = Randomize::instance();
+  for (std::size_t i = 0; i < N; ++i) {
+    const HypernodeID hn = random.getRandomInt(0, hg.currentNumNodes() - 1);
+    const PartitionID from = hg.partID(hn);
+    const PartitionID to = random.getRandomInt(0, k - 1);
+    if (from != to) { // otherwise changeNodePart crashes
+      if (qg.update(hn, from, to)) { // expect behaviour: shouldn't crash ...
+        hg.changeNodePart(hn, from, to);
+      }
+    }
+  }
 }
 
 TEST(QUOTIENT_GRAPH, HasCorrectSize) {
@@ -95,33 +115,43 @@ TEST(QUOTIENT_GRAPH, NullMovesAreLegal) {
   }
 }
 
-TEST(QUOTIENT_GRAPH, MonkeyMovesDontCrashAndRemainAcyclic) {
+TEST(QUOTIENT_GRAPH, MonkeyMovesDontCrashAndRemainsAcyclic) {
   Hypergraph hg = _loadHypergraph("test_instances/acyclic.hgr");
   _applyAcyclic4Partition(hg);
   QuotientGraph<DFSCycleDetector> qg(hg, _createContext(4));
 
-  Randomize& random = Randomize::instance();
   constexpr std::size_t N = 128;
-  constexpr std::size_t K = 4;
-  for (std::size_t i = 0; i < N; ++i) {
-    const HypernodeID hn = random.getRandomInt(0, hg.currentNumNodes() - 1);
-    const PartitionID from = hg.partID(hn);
-    const PartitionID to = random.getRandomInt(0, K - 1);
-    if (from != to) { // otherwise changeNodePart crashes
-      if (qg.update(hn, from, to)) { // expect behaviour: shouldn't crash ...
-        hg.changeNodePart(hn, from, to);
-      }
-    }
-  }
+  constexpr PartitionID k = 4;
+  _performMonkeyMoves(hg, qg, k, N);
   ASSERT_TRUE(qg.isAcyclic());
 }
 
-// not supported (and not needed) for now
-/*TEST(QUOTIENT_GRAPH, CyclicInitialPartitionHasCyclicQuotientGraph) {
-  Hypergraph  hg = _loadHypergraph("test_instances/acyclic.hgr");
-  _applyCyclic4Partition(hg);
-  QuotientGraph<DFSCycleDetector> qg(hg, _createContext(4));
-  ASSERT_FALSE(qg.isAcyclic());
-}*/
+TEST(QUOTIENT_GRAPH, C880_K128_InitiallyAcyclic) {
+  constexpr PartitionID k = 128;
+  Hypergraph hg = _loadHypergraph("test_instances/c880.hgr");
+  _partitionUsingTopologicalOrdering(hg, k);
+  QuotientGraph<DFSCycleDetector> qg(hg, _createContext(k));
+  ASSERT_TRUE(qg.isAcyclic());
+}
+
+TEST(QUOTIENT_GRAPH, C880_K64_MonkeyMovesDontCrashAndRemainsAcyclic) {
+  constexpr PartitionID k = 64;
+  constexpr std::size_t N = 1024;
+  Hypergraph hg = _loadHypergraph("test_instances/c880.hgr");
+  _partitionUsingTopologicalOrdering(hg, k);
+  QuotientGraph<DFSCycleDetector> qg(hg, _createContext(k));
+  _performMonkeyMoves(hg, qg, k, N);
+  ASSERT_TRUE(qg.isAcyclic());
+}
+
+TEST(QUOTIENT_GRAPH, C880_K128_MonkeyMovesDontCrashAndRemainsAcyclic) {
+  constexpr PartitionID k = 128;
+  constexpr std::size_t N = 1024;
+  Hypergraph hg = _loadHypergraph("test_instances/c880.hgr");
+  _partitionUsingTopologicalOrdering(hg, k);
+  QuotientGraph<DFSCycleDetector> qg(hg, _createContext(k));
+  _performMonkeyMoves(hg, qg, k, N);
+  ASSERT_TRUE(qg.isAcyclic());
+}
 } // namespace dag
 } // namespace kahypar
