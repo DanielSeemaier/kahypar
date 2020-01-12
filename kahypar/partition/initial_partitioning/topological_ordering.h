6bac30639c1ada08e211ed7f3ceadab797a676cc
Daniel Seemaier <daniel@seemaier.de>
2020-01-12 22:24:53 +0100

    misc

--- kahypar/partition/initial_partitioning/topological_ordering.h
+++ kahypar/partition/initial_partitioning/topological_ordering.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <kahypar/partition/refinement/acyclic_2way_km1_refiner.h>
 
 #include "kahypar/definitions.h"
 #include "kahypar/partition/initial_partitioning/i_initial_partitioner.h"
@@ -46,6 +47,34 @@ class TopologicalOrderingInitialPartitioner : public IInitialPartitioner,
       }
       ASSERT(p < _context.partition.k, "Could not find a non-overloaded partition for node" << hn);
       _hg.setNodePart(hn, p);
+    }
+
+    _hg.initializeNumCutHyperedges();
+
+    if (_context.partition.k == 2) {
+      Metrics current_metrics = {metrics::hyperedgeCut(_hg),
+                                 metrics::km1(_hg),
+                                 metrics::imbalance(_hg, _context)};
+      HyperedgeWeight previous_km1 = current_metrics.km1;
+      UncontractionGainChanges changes{}; // dummy
+
+      do {
+        previous_km1 = current_metrics.km1;
+
+        AcyclicTwoWayKMinusOneRefiner local_search_refiner(_hg, _context);
+        local_search_refiner.initialize(0);
+
+        std::vector<HypernodeID> local_search_refinement_nodes;
+        for (const HypernodeID& hn : _hg.nodes()) {
+          if (_hg.isBorderNode(hn)) {
+            local_search_refinement_nodes.push_back(hn);
+          }
+        }
+
+        local_search_refiner.refine(local_search_refinement_nodes, {0, 0}, changes, current_metrics);
+        //local_search_refiner.printSummary();
+        LOG << "Result of 2Way refinement:" << previous_km1 << "-->" << current_metrics.km1;
+      } while (0.99 * previous_km1 > current_metrics.km1);
     }
   }
 };
